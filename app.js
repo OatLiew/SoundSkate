a3313e3703f6af371029b04ca842db895a9a150f
Oat Liewsrisuk <oat@Oats-MacBook-Pro.local>
2014-06-28 13:37:04 -0700

    fixed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.factory('myService', function($http) {
     async: function() {
       if ( !promise ) {
         promise = $http.get('search.php').then(function (response) {
-          //console.log(response);
+          console.log(response);
           return response.data;
         });
       }
@@ -26,7 +26,15 @@ app.factory('myService', function($http) {
 //   });
 // });
 
-// app.controller('Hello', function Hello($scope, $http) {
+//the Json datas are inserted in result
+app.controller('MainCtrl', function( myService,$scope) {
+   myService.async().then(function(d) {
+    $scope.result = d;
+  });
+});
+
+
+// app.controller('MainCtrl', function Hello($scope, $http) {
   
 //     $http.get('http://rest-service.guides.spring.io/greeting').
 //         success(function(data) {
