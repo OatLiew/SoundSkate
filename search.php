3cd06bb0145ddb633dbdab1da00060dadc4fa5c7
Oat Liewsrisuk <oat@Oats-MacBook-Pro.local>
2014-06-28 14:01:12 -0700

    fixed

--- search.php
+++ search.php
@@ -4,24 +4,20 @@ try {
   $conn = new PDO('mysql:host=mydb.cpuioo9gx1rx.us-west-2.rds.amazonaws.com;dbname=myDB', 
   'infor344user', 
   'password');
-  
-  //SELECT * FROM NBAplayers
-  //SELECT * FROM SoundSkate
+
   $stmt = $conn->prepare('SELECT * FROM NBAplayers');
   $stmt->execute();
-
-  $result = $stmt->fetchAll();
-
+ 
+  $result = $stmt->fetchAll();  
   $arr = array();
     if ( count($result) ) {
       foreach($result as $row) {
         $arr[] = $row;
       } 
     }
-
     //convert the number type from text to number and encode in Json format
     echo(json_encode($arr,JSON_NUMERIC_CHECK));
-
+    
   } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
   }
